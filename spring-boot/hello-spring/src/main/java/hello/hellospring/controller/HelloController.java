c2d0107ea7c5f99423570208a2561af4dfab53df
yooonar <aryooon33@gmail.com>
2020-12-08 02:08:33 +0900

    MVC + Template engine

--- spring-boot/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ spring-boot/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -15,5 +16,11 @@ public class HelloController {
 
         // /src/main/resources/templates/hello.html 파일을 찾아서 연결해라.
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }

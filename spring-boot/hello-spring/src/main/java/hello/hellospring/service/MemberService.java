3f93bf4ca50cce2734b6ec0f2be705bcd486e381
yooonar <aryooon33@gmail.com>
2020-12-09 00:36:50 +0900

    Member Service - 3단계 Optional 소스 정리

--- spring-boot/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ spring-boot/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -15,6 +15,10 @@ public class MemberService {
      */
     public Long join(Member member) {
 
+        // 아래와 같이 함수 안에 따로 로직이 있는 경우 따로 메소드로 뽑는 것이 좋다. 단축키: 영역 드래그 + option + T -> Extract Method
+        // validateDuplicateMember() 메소드로 새로 추가됨
+        validateDuplicateMember(member); // 중복 회원 검증
+
         /*
         // 같은 이름이 있는 중복 회원은 가입 불가
         Optional<Member> result = memberRepository.findByName(member.getName());
@@ -25,7 +29,6 @@ public class MemberService {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
         // orElseGet : 값이 있으면 꺼내고 없으면 해당 메소드를 실행해라.
-        */
 
         // 위와 같은 문장 Optional<Member> result = memberRepository.findByName(member.getName()); 에서
         // Optional 을 바로 반환하는 게 별로 좋진 않다.
@@ -36,7 +39,15 @@ public class MemberService {
                 });
         // 어차피 memberRepository.findByName(member.getName()) 를 반환하면 Optional 이기 때문에 바로 ifPresent 를 사용할 수 있다.
 
+        */
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 }

f8b5454a8c55dc9a2448de95be5fe0ef1b8408ba
yooonar <aryooon33@gmail.com>
2020-12-12 02:52:35 +0900

    jdbcTemplate

---
+++ spring-boot/hello-spring/src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,85 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository {
+
+    // jdbc 를 쓰기 위해선 JdbcTemplate 이 필요하다.
+    private final JdbcTemplate jdbcTemplate;
+
+    // 생성자가 하나만 있으면 @Autowired 는 생략 가능하다.
+    @Autowired
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+
+        // SimpleJdbcInsert 가 insert 문을 대신 만들어줌
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        member.setId(key.longValue());
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        List<Member> result = jdbcTemplate.query("SELECT * FROM member WHERE id = ?", memberRowMapper());
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = jdbcTemplate.query("SELECT * FROM member WHERE name = ?", memberRowMapper());
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+
+            return member;
+        };
+        /*
+        // 람다식이 아닌 기본 자바 형식
+        return new RowMapper<Member>() {
+            @Override
+            public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+
+                return member;
+            }
+        };
+         */
+    }
+}

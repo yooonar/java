d17e78a8eddcc4c9602f2b6daed1c2104359e0d5
yooonar <aryooon33@gmail.com>
2020-12-18 19:27:20 +0900

    회원 가입 테스트 스프링 문법으로 변환 - ApplicationContext

--- spring-boot/core/src/main/java/hello/core/MemberApp.java
+++ spring-boot/core/src/main/java/hello/core/MemberApp.java
@@ -1,12 +1,24 @@
 package hello.core;
 
 import hello.core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // 회원 테스트
 public class MemberApp {
 
     public static void main(String[] args) {
 
+        // 스프링 문법으로 구현된 소스
+        // ApplicationContext : 스프링 컨텍스트 (@Bean 관리)
+        // AppConfig 에 있는 환경 설정 정보를 가지고 스프링 빈에 넣어 관리함
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 스프링 컨테이너를 통해 찾아옴. getBean("메소드명", 반환타입)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        /*
+        // 기본 자바 문법으로 구현된 소스
         // 의존성 주입
         AppConfig appConfig = new AppConfig();
 
@@ -14,6 +26,7 @@ public class MemberApp {
         // MemberService memberService = new MemberServiceImpl();
         // DI - 의존성 주입
         MemberService memberService = appConfig.memberService();
+         */
 
         // memberA 회원
         Member member = new Member(1L, "MemberA", Grade.VIP);

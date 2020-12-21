7b2e7a90e1ece6735468b0be8d6bffa90f0ca31a
yooonar <aryooon33@gmail.com>
2020-12-21 14:31:36 +0900

    스프링 빈 상속 관계 - 부모 타입으로 조회 시 자식이 둘 이상 있으면 중복 오류 발생

---
+++ spring-boot/core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,46 @@
+package hello.core.beanfind;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextExtendsFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+    @Test
+    @DisplayName("부모 타입으로 조회 시 자식이 둘 이상 있으면 중복 오류가 발생한다.")
+    void findBeanByParentTypeDuplicate() {
+        // 오류 발생
+        // DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
+
+        assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(DiscountPolicy.class));
+    }
+
+    // DiscountPolicy 타입이 두 개 존재하는 클래스
+    @Configuration
+    static class TestConfig {
+
+        // 정률 할인
+        @Bean
+        public DiscountPolicy rateDiscountPolicy() {
+            return new RateDiscountPolicy();
+        }
+
+        // 정액 할인
+        @Bean
+        public DiscountPolicy fixDiscountPolicy() {
+            return new FixDiscountPolicy();
+        }
+    }
+}

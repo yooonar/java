140d911c386b4ac35c6adf1d5ecd8ae750fa70d6
yooonar <aryooon33@gmail.com>
2020-12-28 14:41:07 +0900

    xml 형식으로 스프링 빈 조회

---
+++ spring-boot/core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,24 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+        // MemberService 빈 조회
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        // memberService 가 MemberService.class 의 인스턴스인지?
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}

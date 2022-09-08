76348ccd48e88a9762238c61652deb92af304b92
yooonar <aryooon33@gmail.com>
2022-09-09 06:48:14 +0900

    회원 웹 기능 - 회원 등록 폼 추가

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 // 스프링이 @Controller 어노테이션을 보고 처음 실행할 때 MemberController 객체를 생성해서 대기함
 // 이것을 스프링 컨테이너에서 스프링 빈이 관리된다고 한다.
@@ -15,5 +16,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm"; // templates/members/createMemberForm.html
     }
 }

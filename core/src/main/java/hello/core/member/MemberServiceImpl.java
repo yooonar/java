da082d4bcf804133e7cb2245950e2691f39ffa48
yooonar <aryooon33@gmail.com>
2022-09-15 18:44:41 +0900

    회원 가입 조회 - DIP 위반 관련 주석 추가

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,9 +4,11 @@ public class MemberServiceImpl implements MemberService {
     // 관례: 구현체가 하나만 있는 경우 인터페이스명 뒤에 Impl 을 붙여준다.
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    // 실제 할당하는 부분이 구현체(MemoryMemberRepository)를 의존하고 있다.
-    // 결과적으로 MemberServiceImpl는 MemberRepository(추상화), MemoryMemberRepository(구체화) 둘 다 의존하게 되어 DIP를 위반하고 있다.
-
+    /*
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    실제 할당하는 부분이 구현체(MemoryMemberRepository)를 의존하고 있다.
+    결과적으로 MemberServiceImpl는 MemberRepository(추상화), MemoryMemberRepository(구체화) 둘 다 의존하게 되어 DIP를 위반하고 있다.
+    */
     @Override
     public void join(Member member) {
         memberRepository.save(member);

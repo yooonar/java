8033029883bde39f1fe4374357430a03e41ad33e
yooonar <aryooon33@gmail.com>
2022-09-21 13:23:30 +0900

    주문 - 등급 별 할인 금액 적용(정액 할인)

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); // 회원 찾기
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 고정할인 정책
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId); // 회원 찾기
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 회원 등급에 따른 할인 금액
+
+        return new Order(memberId, itemName, itemPrice, discountPrice); // 주문 생성
+    }
+}

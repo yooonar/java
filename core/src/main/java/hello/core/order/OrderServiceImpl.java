64566dd160634e2f494601dd5d366dc03075f29a
yooonar <aryooon33@gmail.com>
2022-09-21 16:43:21 +0900

    실제 주문 및 테스트 - 등급 별 할인 금액 적용(정률 할인)

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository(); // 회원 찾기
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 고정할인 정책
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 고정할인 정책. 인터페이스 + 구체화에 의존
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 정률할인 정책. 인터페이스 + 구체화에 의존
+    private DiscountPolicy discountPolicy; // 인터페이스 의존
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
